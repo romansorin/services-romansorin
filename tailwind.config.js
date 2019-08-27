f02c246eac25553321028780cd4d0240ffff22cb
Roman Sorin <roman@romansorin.com>
2019-08-26 22:54:49 -0400

    PricingCard styling class, added extensions in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,29 @@
 module.exports = {
   theme: {
     container: {
-      center:true
+      center: true
     },
     extend: {
       fontFamily: {
         display: ['Poppins', 'sans-serif'],
-        body: ['Open Sans', 'sans-serif'],
+        body: ['Open Sans', 'sans-serif']
       },
       colors: {
         softwhite: '#f9f9f9',
         blackish: '#111111',
         themeDarkBlue: '#6190e8',
         themeLightBlue: '#a7bfe8',
+        themeGreen: '#18C65D',
         darkgrey: '#343434',
         lightgrey: '#6f6f6f',
         lightergrey: '#646464',
         lightestgrey: '#e4e4e4'
       },
-    },
+      fontSize: {
+        '2.5xl': '1.75rem'
+      }
+    }
   },
   variants: {},
-  plugins: [],
+  plugins: []
 }
