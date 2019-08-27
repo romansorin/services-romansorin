971845ec35857c26ce8c1e2d8dc23b807ea5be98
Roman Sorin <roman@romansorin.com>
2019-08-26 22:54:16 -0400

    Finished PricingCard component

--- src/components/PricingCard.js
+++ src/components/PricingCard.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react'
 
-import {Button} from './index'
+import { ButtonLink } from './index'
 
 export default class PricingCard extends Component {
-  render() {
-    const { name, price, details, buttonText, buttonClasses, children, classes } = this.props
+  render () {
+    const {
+      name,
+      price,
+      buttonText,
+      buttonClasses,
+      children,
+      link,
+      classes
+    } = this.props
+
+    const styles = {
+      ...this.props.styles,
+      width: '310px'
+    }
+
     return (
-      <div className={`bg-softwhite max-w-md rounded overflow-hidden shadow-lg ${classes}`}>
-        <div className="px-8 py-12">
-          <h3 className="font-medium font-display tracking-wide text-blackish text-2xl -mb-1">{name}</h3>
-          <h3 className="font-medium tracking-wide font-display text-themeDarkBlue text-3xl mb-2">{price}</h3>
-          <p className="py-4 my-4 border-t border-lightestgrey leading-looser font-regular border-b text-lightergrey text-xs">
-            {children}
-          </p>
-          <Button >{buttonText}</Button>
-        </div>
+      <div
+        className={`bg-softwhite px-10 py-10 rounded-sm overflow-hidden pricing-card ${classes}`}
+        style={styles}
+      >
+        <h3 className='font-medium standard font-display tracking-wide text-blackish text-2xl -mb-1'>
+          {name}
+        </h3>
+        <h3 className='font-semibold tracking-wide font-display text-themeDarkBlue text-2.5xl mb-2'>
+          {price}
+        </h3>
+        <p
+          style={{ fontSize: '0.85rem' }}
+          className='h-48 pb-2 pt-3 mb-6 mt-5 border-t border-lightestgrey leading-looser font-medium border-b text-lightergrey text-xs'
+        >
+          {children}
+        </p>
+        <ButtonLink
+          link={link}
+          styles={`border rounded py-2 px-5 text-sm shadow-md font-semibold tracking-wider ${buttonClasses}`}
+        >
+          {buttonText}
+        </ButtonLink>
       </div>
     )
   }
