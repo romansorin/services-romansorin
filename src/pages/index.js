071dfd758e1c6c304bed04f87a93bf165aa83a49
Roman Sorin <roman@romansorin.com>
2019-08-26 22:55:09 -0400

    PricingCard mockup => production on index, pricingPlans object

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,7 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
-import plans from '../images/plans.svg'
-
-import {
-  Button,
-  Hero,
-  Background,
-  PricingCard,
-  FAQCard,
-  Header,
-  Footer,
-  SEO,
-} from '../components'
 import { useStaticQuery, graphql } from 'gatsby'
+
+import { Hero, Background, PricingCard, Header, SEO } from '../components'
 
 import '../css/styles.css'
 
@@ -28,31 +16,68 @@ const IndexPage = () => {
     }
   `)
 
+  const pricingPlans = {
+    standard: {
+      classes: 'ml-auto',
+      styles: { zIndex: 1 },
+      name: 'Standard plan',
+      buttonText: 'try for free'.toUpperCase(),
+      buttonClasses: 'bg-themeGreen text-softwhite',
+      price: '$100/month',
+      link: 'https://www.romansorin.com/contact',
+      features: [
+        'Unlimited sites w/ SSL',
+        'Unlimited storage',
+        '24/7 support',
+        '99.99%',
+        'Managed and deployed by us'
+      ]
+    },
+    custom: {
+      classes: 'mr-auto my-auto',
+      styles: { paddingTop: '2rem', paddingBottom: '2rem' },
+      name: 'Custom plan',
+      buttonText: 'contact us'.toUpperCase(),
+      buttonClasses: 'border-lightgrey text-lightgrey',
+      price: "Let's chat",
+      link: 'https://www.romansorin.com/contact',
+      features:
+        'Flexible options and pricing for businesses, startups, or users with advanced needs and stricter budgets.'
+    }
+  }
+
   return (
     <>
-      <SEO title="Home" />
-      <Background className="hero-svg">
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <Hero
-          title="Unlimited sites. Unlimited storage."
-          subtitle="And so much more, all for one price."
-        >
-          <p className="text-center text-softwhite italic font-body text-sm font-light">
-            Get your first month completely free.
-          </p>
-          <div className="mt-8 container mx-auto flex flex-row">
-            <img className="mx-auto" src={plans} alt="Pricing plans" />
-            <PricingCard name="Standard plan" price="$100/month">Flexible options and pricing for businesses, startups, or users with advanced needs and stricter budgets.</PricingCard>
-          </div>
-        </Hero>
-      </Background>
-      <div className="container mx-auto">
-        <FAQCard />
-        {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          <Image />
-        </div> */}
+      <SEO title='Home' />
+      <div className='mb-64'>
+        <Background className='hero-svg'>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <Hero
+            title='Unlimited sites. Unlimited storage.'
+            subtitle='And so much more, all for one price.'
+          >
+            <p className='text-center text-softwhite italic font-body text-sm font-light'>
+              Get your first month completely free.
+            </p>
+            <div className='mt-24 container mx-auto flex flex-row'>
+              <PricingCard {...pricingPlans.standard}>
+                <ul>
+                  {pricingPlans.standard.features.map(feature => (
+                    <li key={feature}>- {feature}</li>
+                  ))}
+                </ul>
+              </PricingCard>
+              <PricingCard {...pricingPlans.custom}>
+                {pricingPlans.custom.features}
+              </PricingCard>
+            </div>
+          </Hero>
+        </Background>
       </div>
-      <Footer />
+      <div className='container mx-auto mb-16'>
+        <h1 className='text-4xl font-bold'>More coming soon.</h1>
+      </div>
+      {/* <Footer /> */}
     </>
   )
 }
